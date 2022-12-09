c19eb9ad636340b14a13e17391b8477da48d8694
elobriere <elodie.briere@univ-paris1.fr>
2022-12-09 12:22:42 +0100

    [IMP] Logger la synchronisation globale sur la sortie standard.

--- html/cron/synchro_global_esign.php
+++ html/cron/synchro_global_esign.php
@@ -3,15 +3,15 @@
 	require_once dirname(__FILE__,2).'/class/decree.php';
 	require_once dirname(__FILE__,2).'/class/reference.php';
 
-    elog("début de la synchronisation globale");
+    echo date("d/m/Y H:i:s")." début de la synchronisation globale. \n";
 	$ref = new reference($dbcon, $rdbApo);
     $listdecree = $ref->getAllDecreeEnCoursSign();
-    elog(sizeof($listdecree)." demandes à synchroniser.");
+    echo sizeof($listdecree)." demandes à synchroniser.\n";
     foreach($listdecree as $decree)
     {
-        elog ("statut de la demande ".$decree->getid()." avant synchro fin de circuit eSignature : ".$decree->getStatus(false));
+        echo "statut de la demande ".$decree->getid()." avant synchro fin de circuit eSignature : ".$decree->getStatus(false)."\n";
         $decree->synchroEsignatureStatus($decree->getStatus(false));
-        elog ("statut de la demande ".$decree->getid()." après synchro fin de circuit eSignature : ".$decree->getStatus(false));
+        echo "statut de la demande ".$decree->getid()." après synchro fin de circuit eSignature : ".$decree->getStatus(false)."\n";
     }
-    elog("fin de la synchronisation globale");
+    echo date("d/m/Y H:i:s")." fin de la synchronisation globale.\n";
 ?>
