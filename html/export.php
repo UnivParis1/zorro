3cd197154a95bb6423c43048ff128b4b8afb2977
elobriere <elodie.briere@univ-paris1.fr>
2024-05-02 16:18:28 +0200

    [NEW] Proposer l'export de la liste des formations pour lesquelles un arrêté de commission est éditable.

--- html/export.php
+++ html/export.php
@@ -54,6 +54,25 @@
 		exit();
 	}
 
+	if (isset($_POST['formation_liste']))
+	{
+		$ref = new reference($dbcon, $rdbApo);
+		$allmentions = $ref->getAllMentionsCommissions();
+		$list_comp = array_column($ref->getListComp(), 'value', 'code');
+		$list_mention_comp = "mention;composante\n";
+		foreach ($allmentions as $mention => $detail)
+		{
+			$list_mention_comp .= "\"".html_entity_decode($mention)."\";\"".$list_comp[$detail['cmp']]."\"\n";
+		}
+		$doc = fopen(PDF_PATH."mentions_composantes.csv", 'w+');
+		fputs($doc, $list_mention_comp);
+		fclose($doc);
+		header("Content-Type: text/csv");
+		header("Content-disposition: attachment; filename=\"mentions_composantes".date("Ymd").".csv\"");
+		readfile(PDF_PATH."mentions_composantes.csv");
+		exit();
+	}
+
 	$menuItem = 'menu_export';
 	require ("include/menu.php");
 	if (isset($_SESSION['phpCAS']) && array_key_exists('user', $_SESSION['phpCAS']))
@@ -64,9 +83,13 @@
 			<div id="contenu1">
 				<h2>Exports</h2>
 				<form name='formation_president' method='post' action='export.php'>
-					<label>Export Formation / Président de jury pour Gestion des recours</label>
+					<label>Formation / Président de jury pour Gestion des recours</label>
 					<label title="laisser vide pour la totalité">Arrêtés validés depuis</label> <input type='number' name='depuis'> jour(s)
 					<a href="#" target="_blank"><input type='submit' name='formation_president' value='csv'></a>
+				</form>
+				<form name='formation_liste' method='post' action='export.php'>
+					<label>Liste des formations pour la DEVE</label>
+					<a href="#" target="_blank"><input type='submit' name='formation_liste' value='csv'></a>
 				</form>
 			</div>
 		<?php 
