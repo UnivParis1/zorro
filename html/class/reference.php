6f7f5fd03a51793b6c5a7414757a98ea9b752c91
Elodie Briere <ebriere@univ-paris1.fr>
2025-03-10 10:35:06 +0100

    [FIX] Corriger dernier commit non compatible PHP7.

--- html/class/reference.php
+++ html/class/reference.php
@@ -687,7 +687,7 @@ class reference {
 
 	function format_price($price)
 	{
-		return $price != '' && !str_contains($price, ' ') ? number_format($price, 2, '.', ' ') : $price;
+		return $price != '' && is_float($price) ? number_format($price, 2, '.', ' ') : $price;
 	}
 
 	function getRoomsList()
