5820a1048bea1a82e3708f638dd4aeac4539d4a8
elobriere <elodie.briere@univ-paris1.fr>
2025-07-16 10:59:37 +0200

    [FIX] Masquer le bandeau de message lorsqu'aucun message n'est défini.

--- html/create_decree.php
+++ html/create_decree.php
@@ -1158,8 +1158,9 @@
 				$access = true;
 				?>
 				<h2>Modification d'un document</h2>
-				<p class="alerte alerte-modif"><?php echo WARNING_MODIF_MESSAGE; ?> </p>
-				<?php
+				<?php if (defined('WARNING_MODIF_MESSAGE') && WARNING_MODIF_MESSAGE != '') { ?>
+					<p class="alerte alerte-modif"><?php echo WARNING_MODIF_MESSAGE; ?> </p>
+				<?php }
 				//print_r2($mod_decree_fields);
 			}
 			else
